5ed7882a5e1ad43999ee337d8eb699c6b883ac64
prayzzz <prayzzz@outlook.com>
2019-03-30 22:57:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# My New Tab Page
+
+Set a local or web link as your New Tab Page.
+
+No Tracking, no Ads, no Bullshit.
---
+++ const.js
@@ -0,0 +1,3 @@
+const configKey = "my-new-tab-page-url";
+const defaultPage = "https://news.ycombinator.com/";
+// const defaultPage = "file://D:\\Daten\\Development\\my-ntp\\sample.html";
